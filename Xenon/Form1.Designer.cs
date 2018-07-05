991bf60f4de18bfdbdcd711dd09ae541d875c801
pxdnbluesoul <github@nags.me>
2018-07-05 08:05:34 -0600

    Adding to source control.

---
+++ Xenon/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Xenon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.labelUsername = new System.Windows.Forms.Label();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Xenon.Properties.Resources.logo;
+            this.pictureBox1.ImageLocation = "";
+            this.pictureBox1.Location = new System.Drawing.Point(122, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(256, 36);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.Enabled = false;
+            this.textBoxUsername.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxUsername.Location = new System.Drawing.Point(12, 137);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(460, 29);
+            this.textBoxUsername.TabIndex = 1;
+            this.textBoxUsername.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxUsername_KeyDown);
+            // 
+            // labelUsername
+            // 
+            this.labelUsername.AutoSize = true;
+            this.labelUsername.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelUsername.ForeColor = System.Drawing.Color.White;
+            this.labelUsername.Location = new System.Drawing.Point(8, 113);
+            this.labelUsername.Name = "labelUsername";
+            this.labelUsername.Size = new System.Drawing.Size(81, 21);
+            this.labelUsername.TabIndex = 2;
+            this.labelUsername.Text = "Username";
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Enabled = false;
+            this.textBoxPassword.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPassword.Location = new System.Drawing.Point(12, 214);
+            this.textBoxPassword.MaxLength = 2048;
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(460, 29);
+            this.textBoxPassword.TabIndex = 2;
+            this.textBoxPassword.UseSystemPasswordChar = true;
+            this.textBoxPassword.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxPassword_KeyDown);
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPassword.ForeColor = System.Drawing.Color.White;
+            this.labelPassword.Location = new System.Drawing.Point(8, 190);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(77, 21);
+            this.labelPassword.TabIndex = 4;
+            this.labelPassword.Text = "Password";
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Enabled = false;
+            this.buttonLogin.Location = new System.Drawing.Point(197, 283);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(106, 23);
+            this.buttonLogin.TabIndex = 3;
+            this.buttonLogin.Text = "Log On";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripProgressBar1,
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 339);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(484, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.ActiveLinkColor = System.Drawing.Color.Blue;
+            this.toolStripStatusLabel1.AutoSize = false;
+            this.toolStripStatusLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(250, 17);
+            this.toolStripStatusLabel1.Text = "Establishing connection to server...";
+            this.toolStripStatusLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(200, 16);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(119)))), ((int)(((byte)(51)))));
+            this.ClientSize = new System.Drawing.Size(484, 361);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.labelUsername);
+            this.Controls.Add(this.textBoxUsername);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "full.com.bo Xenon";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.Label labelUsername;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ Xenon/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Threading;
+using System.Xml;
+
+namespace Xenon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void SetStatusText(string text)
+        {
+            toolStripStatusLabel1.Text = text;
+        }
+
+        private void SetProgressBar(int value)
+        {
+            toolStripProgressBar1.Value = value;
+        }
+
+        public string BuildURI(string path) // We're doing this for the move to full.com.bo so we only need to change the path in one place.
+        {
+            string CurrentHost = "http://xenon.laravel/";
+            return (CurrentHost + path);
+        }
+
+        private int BuildConfigFile()
+        {
+            XmlDocument newconfig = new XmlDocument();
+            XmlNode configRoot = newconfig.CreateElement("xenon");
+                XmlElement configAuth = newconfig.CreateElement("auth");
+                    XmlAttribute configAuthAccessToken = newconfig.CreateAttribute("access_token");
+                    configAuth.Attributes.Append(configAuthAccessToken);
+                    XmlAttribute configAuthRefreshToken = newconfig.CreateAttribute("refresh_token");
+                    configAuth.Attributes.Append(configAuthRefreshToken);
+                XmlElement configApp = newconfig.CreateElement("app");
+                    XmlAttribute configAppSM5Dir = newconfig.CreateAttribute("sm5dir");
+                    configApp.Attributes.Append(configAppSM5Dir);
+                    XmlAttribute configAppDataDir = newconfig.CreateAttribute("datadir");
+                    configApp.Attributes.Append(configAppDataDir);
+                    XmlAttribute configAppLocalProfile = newconfig.CreateAttribute("localprofile");
+                    configApp.Attributes.Append(configAppLocalProfile);
+                    XmlAttribute configAppUploadFails = newconfig.CreateAttribute("uploadfails");
+                    configApp.Attributes.Append(configAppUploadFails);
+                    XmlAttribute configAppUploadMachineStats = newconfig.CreateAttribute("uploadmachinestats");
+                    configApp.Attributes.Append(configAppUploadMachineStats);
+            configRoot.AppendChild(configAuth);
+            configRoot.AppendChild(configApp);
+            newconfig.AppendChild(configRoot);
+            newconfig.Save("config.xml");
+            return 1;
+        }
+
+        public int GetServerStatus() // This is run in a thread to verify the server's around and listening.
+        {
+            // Check for a config file and make one if not present.
+
+            XmlDocument config = new XmlDocument();
+            try
+            {
+                config.Load("config.xml");
+            }
+            catch (System.IO.FileNotFoundException e)
+            {
+                int build = BuildConfigFile();
+            }
+            catch (XmlException e)
+            {
+                int build = BuildConfigFile();
+            }
+            backgroundWorker1.ReportProgress(50);
+
+            HttpWebRequest httpReq = (HttpWebRequest)WebRequest.Create(BuildURI("api/ok"));
+            httpReq.AllowAutoRedirect = false;
+            try
+            {
+                HttpWebResponse httpRes = (HttpWebResponse)httpReq.GetResponse();
+                return (int)httpRes.StatusCode;
+            }
+            catch (WebException e) // 4xx and 5xx status codes return as an Exception
+            {
+                HttpWebResponse httpRes = (HttpWebResponse)e.Response;
+                if (e.Status.ToString() == "ConnectFailure") // No response at all isn't handled via an HTTP status code.
+                { return 0; }
+                return (int)httpRes.StatusCode;
+            }
+        }
+
+        private string GetCatalogHash()
+        {
+            WebClient client = new WebClient();
+            Uri uri = new Uri(BuildURI("api/catalog/sha"));
+            return client.DownloadString(uri);
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            e.Result = GetServerStatus();
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            toolStripProgressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if ((int)e.Result == 200)
+            {
+                SetProgressBar(100);
+                textBoxUsername.Enabled = true;
+                textBoxPassword.Enabled = true;
+                buttonLogin.Enabled = true;
+                SetStatusText("Connected.");
+            }
+
+            else if((int)e.Result == 0) // Server's down or no network.
+            {
+                SetProgressBar(0);
+                SetStatusText("Server unavailable.");
+            }
+
+            else
+            {
+                SetProgressBar(0);
+                SetStatusText("Server returned status " + ((int)e.Result) + ".");
+            }
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            SetProgressBar(0);
+            if (textBoxUsername.TextLength > 0 && textBoxPassword.TextLength > 0)
+            {
+                buttonLogin.Enabled = false;
+                textBoxUsername.Enabled = false;
+                textBoxPassword.Enabled = false;
+                SetProgressBar(50);
+                SetStatusText("Logging in...");
+            }
+            else if (textBoxUsername.TextLength == 0)
+            {
+                SetStatusText("Username required.");
+            }
+            else { SetStatusText("Password required."); }
+        }
+
+        private void textBoxUsername_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyValue == 13)
+            {
+                buttonLogin.PerformClick();
+            }
+        }
+
+        private void textBoxPassword_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyValue == 13)
+            {
+                buttonLogin.PerformClick();
+            }
+        }
+    }
+}
